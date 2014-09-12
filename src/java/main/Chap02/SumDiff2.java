8ce315d505aa2e5ffa880d20ca5022a47a305450
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-12 17:47:12 +0900

    chap02,1,2追加

--- src/java/main/Chap02/SumDiff2.java
+++ src/java/main/Chap02/SumDiff2.java
@@ -4,7 +4,7 @@ public class SumDiff2 {
 
 	public static void main(String[] args) {
       System.out.println("82 + 17 = " + (82 + 17));
-      System.out.println("82 - 17 = " + (82 - 17));
+      System.out.println("2 - 17 = " + (82 - 17));
 	}
 
 }
