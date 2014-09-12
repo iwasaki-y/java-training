5f0e45555afa7048be58905de01974b5b084b3fe
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-12 18:36:44 +0900

    chap02,5,1追加

---
+++ src/java/main/Chap02/ScanInteger.java
@@ -0,0 +1,16 @@
+package Chap02;
+
+import java.util.Scanner;
+
+public class ScanInteger {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("整数値:");
+        int x = stdIn.nextInt();
+        
+        System.out.println(x + "と入力しましたね。");
+	}
+
+}
