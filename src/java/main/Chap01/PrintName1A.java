9681d22915bc44025a4c0f41db2483829a847167
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-12 16:26:52 +0900

    chap01,4,1追加

---
+++ src/java/main/Chap01/PrintName1A.java
@@ -0,0 +1,13 @@
+package Chap01;
+
+public class PrintName1A {
+
+	public static void main(String[] args) {
+		System.out.println("柴");
+		System.out.println("田");
+		System.out.println("望");
+		System.out.println("洋");
+
+	}
+
+}
