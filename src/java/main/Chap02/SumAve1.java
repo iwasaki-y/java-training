cf7bf65ee85a7ea687d688c7457eb03590c6653a
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-12 17:50:42 +0900

    chap02,2,1追加

--- src/java/main/Chap02/SumAve1.java
+++ src/java/main/Chap02/SumAve1.java
@@ -9,7 +9,7 @@ public class SumAve1 {
 		x = 63;
 		y = 18;
 		
-		System.out.println("xの値" + x + "です。");
+		System.out.println("xの値は" + x + "です。");
 		System.out.println("yの値は" + y + "です。");
 		System.out.println("合計は" + (x + y) + "です。");
 		System.out.println("平均は" + (x + y) /2 + "です。");
