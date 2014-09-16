fac066fdbae4fa5e9033b628ff3b707553f5cd55
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-16 14:37:56 +0900

    chap03,12,2追加

---
+++ src/java/main/Chap03/Max2B.java
@@ -0,0 +1,18 @@
+package Chap03;
+
+import java.util.Scanner;
+
+public class Max2B {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("実数a:"); double a = stdIn.nextDouble();
+		System.out.print("実数b:"); double b = stdIn.nextDouble();
+		
+		double max = a > b ? a : b;
+		System.out.println("大きいほうの値は" + max + "です。");
+
+	}
+
+}
