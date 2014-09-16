4f8cb64dc60579af7052d90c0ef9e8c0ea60331c
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-16 17:23:29 +0900

    chap04,4,1追加

---
+++ src/java/main/Chap04/EnumScope.java
@@ -0,0 +1,27 @@
+package Chap04;
+
+import java.util.Scanner;
+
+public class EnumScope {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("整数A:"); int a = stdIn.nextInt();
+		System.out.print("整数B:"); int b = stdIn.nextInt();
+		
+		if (a > b) {
+			int t = a;
+			a = b;
+			b = t;
+		}
+		
+		do {
+			System.out.print(a + " ");
+			a = a + 1;
+		} while (a <= b);
+		System.out.println();
+
+	}
+
+}
