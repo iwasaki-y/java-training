686ebc7930b3f0a84748fe6af8f1bb34a068c6fd
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-16 17:48:24 +0900

    chap04,6,1追加

---
+++ src/java/main/Chap04/CountDown2Pre.java
@@ -0,0 +1,23 @@
+package Chap04;
+
+import java.util.Scanner;
+
+public class CountDown2Pre {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("カウントダウンします。");
+		int x;
+		do {
+			System.out.print("正の整数値:");
+			x = stdIn.nextInt();
+		} while (x <= 0);
+		
+		while (x >= 0)
+			System.out.println(--x);
+		
+		System.out.println("xの値は" + x + "になりました。");
+	}
+
+}
