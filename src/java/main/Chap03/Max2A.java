2b8d6c39518528b3f5b3f4ef16f642321509b88c
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-16 14:30:13 +0900

    chap03,12,1追加

---
+++ src/java/main/Chap03/Max2A.java
@@ -0,0 +1,23 @@
+package Chap03;
+
+import java.util.Scanner;
+
+public class Max2A {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("実数a："); double a = stdIn.nextDouble();
+		System.out.print("実数b:"); double b = stdIn.nextDouble();
+		
+		double max;
+		if (a > b)
+			max = a;
+		else
+			max = b;
+		
+		System.out.println("大きいほうの値は" + max + "です。");
+
+	}
+
+}
