2213847024163906dfe7c5903233d48d6d0d1691
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-16 18:16:51 +0900

    chap04,8,1追加

---
+++ src/java/main/Chap04/PutAsteriskAlt1.java
@@ -0,0 +1,27 @@
+package Chap04;
+
+import java.util.Scanner;
+
+public class PutAsteriskAlt1 {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("何個表示しますか:");
+		int n = stdIn.nextInt();
+		
+		if (n > 0) {
+			int i = 0;
+			while (i < n) {
+				if (i % 2 == 0)
+					System.out.print('*');
+				else
+					System.out.print('+');
+				i++;
+			}
+			System.out.println();
+		}
+
+	}
+
+}
