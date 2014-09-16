a1ace501fb018fe0b10bc7164debd0918ee1050e
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-16 14:44:53 +0900

    chap03,13,1

---
+++ src/java/main/Chap03/Diff2A.java
@@ -0,0 +1,22 @@
+package Chap03;
+
+import java.util.Scanner;
+
+public class Diff2A {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("整数a:"); int a = stdIn.nextInt();
+		System.out.print("整数b:"); int b = stdIn.nextInt();
+		
+		int diff;
+		if (a >= b)
+			diff = a - b;
+		else
+			diff = b - a;
+		System.out.println("それらの差は" + diff + "です。");
+
+	}
+
+}
