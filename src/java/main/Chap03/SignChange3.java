8d024bf70bfb40dff204c8d9325b2008acfbe743
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-16 13:13:15 +0900

    chap03,6,3追加

---
+++ src/java/main/Chap03/SignChange3.java
@@ -0,0 +1,24 @@
+package Chap03;
+
+import java.util.Scanner;
+
+public class SignChange3 {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("整数：");
+		int n = stdIn.nextInt();
+		
+		if (n == 1)
+			System.out.println("それは1です。");
+		else if (n == 2)
+			System.out.println("それは2です。");
+		else if (n  == 3)
+			System.out.println("それは3です。");
+		else
+			;
+
+	}
+
+}
