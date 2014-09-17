eddcbe4fb8c2d016d65a32c07b07a6dc911d63b4
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-17 09:39:33 +0900

    chap04,11,1追加

---
+++ src/java/main/Chap04/PutAsteriskFor.java
@@ -0,0 +1,20 @@
+package Chap04;
+
+import java.util.Scanner;
+
+public class PutAsteriskFor {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("何個*を表示しますか:");
+		int n = stdIn.nextInt();
+		
+		if (n > 0) {
+			for (int i = 0; i < n; i++)
+				System.out.print('*');
+			System.out.println();		}
+
+	}
+
+}
