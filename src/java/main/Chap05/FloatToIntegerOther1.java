71b662572ca39178f6934004bab9ded608e5a74a
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-17 15:16:40 +0900

    chap05,7,2追加

---
+++ src/java/main/Chap05/FloatToIntegerOther1.java
@@ -0,0 +1,13 @@
+package Chap05;
+
+public class FloatToIntegerOther1 {
+
+	public static void main(String[] args) {
+		short a = 1;
+//		byte b = a;
+		
+		System.out.println("a =" + a);
+
+	}
+
+}
