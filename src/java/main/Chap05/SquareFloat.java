a002be4109c9622ed95903c0524f8536580f4a72
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-17 15:33:40 +0900

    chap05,9,1追加

---
+++ src/java/main/Chap05/SquareFloat.java
@@ -0,0 +1,14 @@
+package Chap05;
+
+public class SquareFloat {
+
+	public static void main(String[] args) {
+		System.out.println("  x      xの2乗");
+		System.out.println("----------------");
+		
+		for (float x = 0.0F; x <= 1.0F; x += 0.001F)
+			System.out.printf("%5.3f %10.7f\n", x, x * x);
+
+	}
+
+}
