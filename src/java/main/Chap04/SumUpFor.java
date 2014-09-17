aec31810cd02b2b38d1863297c2f91517feeb766
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-17 09:58:43 +0900

    chap04,14,1追加

---
+++ src/java/main/Chap04/SumUpFor.java
@@ -0,0 +1,23 @@
+package Chap04;
+
+import java.util.Scanner;
+
+public class SumUpFor {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("1からnまでの和を求めます。");
+		int n;
+		do {
+			System.out.print("ｎの値:");
+			n = stdIn.nextInt();
+		} while (n <= 0);
+		
+		int sum = 0;
+		for (int i = 1; i <= n; i++)
+			sum += i;
+		System.out.println("1から" + n + "までの和は" + sum + "です。");
+	}
+
+}
