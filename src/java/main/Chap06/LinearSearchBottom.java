e4abb2309c3af142fd78b99dfa0f0c96fd6f4630
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-17 17:39:53 +0900

    chap06,8,2追加

---
+++ src/java/main/Chap06/LinearSearchBottom.java
@@ -0,0 +1,34 @@
+package Chap06;
+
+import java.util.Scanner;
+
+public class LinearSearchBottom {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("要素数:");
+		int n = stdIn.nextInt();
+		int[] a = new int[n];
+		
+		for (int j = 0; j < n; j++) {
+			System.out.print("a[" + j + "] = ");
+			a[j] = stdIn.nextInt();
+		}
+		
+		System.out.print("探す数値:");
+		int key = stdIn.nextInt();
+		
+		int i;
+		for (i = n - 1; i >= 0; i--)
+			if (a[i] == key)
+				break;
+		
+		if (i >= 0)
+			System.out.print("それはa[" + i + "]にあります。");
+		else
+			System.out.print("それはありません。");
+
+	}
+
+}
