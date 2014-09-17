2c55a5ed0afd590d4cfe482189ed4b87f3778737
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-17 10:33:16 +0900

    chap04,17,1追加

---
+++ src/java/main/Chap04/PutAsterisk5A.java
@@ -0,0 +1,25 @@
+package Chap04;
+
+import java.util.Scanner;
+
+public class PutAsterisk5A {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("何個*を表示しますか:");
+		int n = stdIn.nextInt();
+		
+		if (n > 0) {
+			for (int i = 0; i < n; i++) {
+				System.out.print('*');
+				if (i % 5 == 4)
+					System.out.println();
+			}
+			if (n % 5 != 0)
+				System.out.println();
+		}
+
+	}
+
+}
