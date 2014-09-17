ae977cf2282b7612ca1fc4af7f57d8bbe8b685cc
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-17 11:35:08 +0900

    chap04,22,1追加

--- src/java/main/Chap04/IsoscelesTriangleLB.java
+++ src/java/main/Chap04/IsoscelesTriangleLB.java
@@ -7,7 +7,7 @@ public class IsoscelesTriangleLB {
 	public static void main(String[] args) {
 		Scanner stdIn = new Scanner(System.in);
 		
-		System.out.println("左下直角の二等辺三角形を表示します。");
+		System.out.println("左下直角の二等辺三角形表示します。");
 		System.out.print("段数は:");
 		int n = stdIn.nextInt();
 		
