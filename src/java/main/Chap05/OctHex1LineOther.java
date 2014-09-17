1008e85198f2386b729c2d994c22637eac2dee08
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-17 14:21:45 +0900

    chap05,2,3追加

---
+++ src/java/main/Chap05/OctHex1LineOther.java
@@ -0,0 +1,16 @@
+package Chap05;
+
+public class OctHex1LineOther {
+
+	public static void main(String[] args) {
+
+		System.out.printf("ABC\n");
+		System.out.printf("%d\n", 12345);
+		System.out.printf("%3d\n", 12345);
+		System.out.printf("%7d\n", 12345);
+		System.out.printf("%5d\n", 123);
+		System.out.printf("%05d\n", 123);
+
+	}
+
+}
