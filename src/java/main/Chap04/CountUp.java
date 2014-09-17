bba69288aab35e15dd31e1c45d9a21b504268491
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-17 09:50:50 +0900

    chap04,13,1追加

---
+++ src/java/main/Chap04/CountUp.java
@@ -0,0 +1,23 @@
+package Chap04;
+
+import java.util.Scanner;
+
+public class CountUp {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.println("カウントアップします。");
+		
+		int x;
+		do {
+			System.out.print("正の整数値:");
+			x = stdIn.nextInt();
+		} while (x <= 0);
+		
+		for (int i = 0; i <= x; i++)
+			System.out.println(i);
+
+	}
+
+}
