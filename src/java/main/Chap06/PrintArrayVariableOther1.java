b06cca51d3d568ec24aedb1ea81eacc4ac9c6fd7
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-18 14:48:38 +0900

    chap06,17,2追加

--- src/java/main/Chap06/PrintArrayVariableOther1.java
+++ src/java/main/Chap06/PrintArrayVariableOther1.java
@@ -3,7 +3,7 @@ package Chap06;
 public class PrintArrayVariableOther1 {
 
 	public static void main (String[] args) {
-		int[] a = {1, 2, 3, 4, 5};
+		int[] a = { 2, 3, 4, 5};
 		int[] b = {6, 5, 4, 3, 2, 1, 0};
 		
 		System.out.println("a = " + a);
