51f3bca43f901d4bda5d08c34a549e5dfe40e527
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-18 14:54:33 +0900

    chap06,17,3追加

---
+++ src/java/main/Chap06/PrintArrayVariableOther2.java
@@ -0,0 +1,13 @@
+package Chap06;
+
+public class PrintArrayVariableOther2 {
+
+	public static void main(String[] args) {
+		final int [] a = new int [5];
+		a[0] = 10;
+		
+		System.out.println("a = " + a);
+
+	}
+
+}
