d765b43c977954988f9c233b4835c13d7174e5f9
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-19 10:39:13 +0900

    chap07,9,1追加

---
+++ src/java/main/Chap07/InverseNumber.java
@@ -0,0 +1,37 @@
+package Chap07;
+
+import java.util.Scanner;
+
+public class InverseNumber {
+	static Scanner stdIn = new Scanner(System.in);
+	
+	static int readPlusInt() {
+		int x;
+		do {
+			System.out.print("正の整数値:");
+			x = stdIn.nextInt();
+		} while (x <= 0);
+		return x;
+	}
+
+	public static void main(String[] args) {
+		int x ;
+		do {
+			int n = readPlusInt();
+			
+			System.out.print("逆から読むと");
+			while (n > 0) {
+				System.out.print(n % 10);
+				n /= 10;
+			}
+			System.out.println("です。");
+			
+			do {
+				System.out.print("もう一度?<Yes・・・1/No・・・0>:");
+				x = stdIn.nextInt();
+			} while (x != 0 && x != 1);
+		} while (x == 1);
+
+	}
+
+}
