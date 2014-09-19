a63a9bcc70a843d739df5ea60083317612c5bfb5
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-19 09:29:13 +0900

    chap07,5,1追加

---
+++ src/java/main/Chap07/Hello.java
@@ -0,0 +1,16 @@
+package Chap07;
+
+public class Hello {
+	
+	static void hello() {
+		System.out.println("こんにちは。");
+	}
+
+	public static void main(String[] args) {
+		hello();
+		hello();
+		hello();
+
+	}
+
+}
