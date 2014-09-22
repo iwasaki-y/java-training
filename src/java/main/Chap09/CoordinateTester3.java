2decdfbdab38265a29ddcaa1e3acf85e3e2cbed9
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-22 15:52:04 +0900

    chap09,1,5追加

---
+++ src/java/main/Chap09/CoordinateTester3.java
@@ -0,0 +1,23 @@
+package Chap09;
+
+import java.util.Scanner;
+
+public class CoordinateTester3 {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("座標は何個:");
+		int n = stdIn.nextInt();
+		
+		Coordinate[] a = new Coordinate[n];
+		
+		for (int i = 0; i < a.length; i++)
+			a[i] = new Coordinate(5.5, 7.7);
+		
+		for (int i = 0; i < a.length; i++)
+			System.out.printf("a[%d] = (%.1f, %.1f)\n", i, a[i].getX(), a[i].getY());
+
+	}
+
+}
