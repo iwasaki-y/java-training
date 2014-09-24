02297fd2dc092ebe39db9bd378eb62d8727560e6
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-24 15:10:16 +0900

    chap10,5,1追加

---
+++ src/java/main/Chap10/IntegerLimits.java
@@ -0,0 +1,23 @@
+package Chap10;
+
+public class IntegerLimits {
+
+	public static void main(String[] args) {
+		System.out.println("整数型の表現範囲");
+		
+		System.out.println("byte 型:"  + Byte.MIN_VALUE
+				             + "~" + Byte.MAX_VALUE);
+		
+		System.out.println("short型:" + Short.MIN_VALUE
+				             + "~" + Short.MAX_VALUE);
+		
+		System.out.println("int  型 :" + Integer.MIN_VALUE
+				             + "~" + Integer.MAX_VALUE);
+		
+		System.out.println("long 型 :" + Long.MIN_VALUE
+				             + "~" + Long.MAX_VALUE);
+		
+
+	}
+
+}
