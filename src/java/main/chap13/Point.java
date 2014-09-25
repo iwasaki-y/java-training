f1083ec493ab82ab7a6844981af572d26d205af5
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-25 17:04:21 +0900

    chap13,3,2追加

---
+++ src/java/main/chap13/Point.java
@@ -0,0 +1,41 @@
+package chap13;
+
+/**
+ * クラスPointは、点を表すクラスです。
+ * このクラスは、図形を表す抽象クラスShapeから派生したクラスです。
+ * フィールドはありません。
+ * @author iwasaki-y
+ * @see    Shape
+ *
+ */
+
+public class Point extends Shape {
+	
+	/**
+	 * 点を生成するコンストラクタです。
+	 * 受け取る引数はありません。
+	 */
+	
+	public Point() {
+		//何も行わない
+	}
+	
+	/**
+	 * メソッドtoStringは、点に関する図形情報を表す文字列を返却します。
+	 * 返却する文字列は、常に"Ponit"です。
+	 * @return 文字列"Point"を返却します。
+	 */
+	
+	public String toString() {
+		return "Point";
+	}
+	
+	/**
+	 * メソッドdrawは、点を描画するメソッドです。
+	 * プラス記号'+'を1個だけ表示して改行します。
+	 */
+	public void draw() {
+		System.out.println('+');
+	}
+
+}
