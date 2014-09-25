b400d0c30215dbb62556020e99960a084134ac48
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-25 09:43:35 +0900

    chap10,9,1追加

---
+++ src/java/main/Chap10/Point2D.java
@@ -0,0 +1,38 @@
+package Chap10;
+
+import java.util.GregorianCalendar;
+import static java.util.GregorianCalendar.*;
+
+public class Point2D {
+	private static int counter = 0;
+	private static int day;
+	
+	private int x = 0;
+	private int y = 0;
+	
+	static {
+		GregorianCalendar today = new GregorianCalendar();
+		day = today.get(DATE);
+	}
+	
+	{
+		if (++counter == day) {
+			System.out.print("当り!!");
+			System.out.printf("今日%d個目の座標が生成されました。\n", counter);
+		}
+	}
+	
+	public Point2D()             { }
+	public Point2D(int x)        { this.x = x; }
+	public Point2D(int x, int y) { this.x = x; this.y = y; }
+	
+	public int getX() { return x; }
+	public int getY() { return y; }
+	
+	public static int getCounter() { return counter; }
+	
+	public String toString() {
+		return "(" + x + ", " + y + ")";
+	}
+
+}
