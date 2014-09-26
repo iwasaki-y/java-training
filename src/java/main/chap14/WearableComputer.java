0673a95486ec21b5b8c23658d844037d542a9cbf
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-26 13:46:07 +0900

    chap14,1,3追加

--- src/java/main/chap14/WearableComputer.java
+++ src/java/main/chap14/WearableComputer.java
@@ -3,11 +3,11 @@ package chap14;
 public class WearableComputer implements Wearable {
 	
 	public void putOn() {
-		System.out.println("コンピューターを起動しました。");
+		System.out.println("コンピュータを起動しました。");
 	}
 	
 	public void putOff() {
-		System.out.println("コンピューターをシャットダウンしました。");
+		System.out.println("コンピュータをシャットダウンしました。");
 	}
 	
 	public void reset() {
