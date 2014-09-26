ff20849567cf3f48ccb9386bc232b82eacea7132
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-26 17:26:13 +0900

    chap14,5,2追加

---
+++ src/java/main/chap14/HeadMountedDisplayTester.java
@@ -0,0 +1,23 @@
+package chap14;
+
+public class HeadMountedDisplayTester {
+
+	public static void main(String[] args) {
+		HeadMountedDisplay hmd = new HeadMountedDisplay();
+		hmd.putOn();
+		hmd.putOff();
+		hmd.changeSkin(Skinnable.LEOPARD);
+		hmd.putSkin();
+		
+		Wearable w = hmd;
+		w.putOn();
+		w.putOff();
+		
+		Skinnable s = hmd;
+		s.changeSkin(Skinnable.BLACK);
+		
+		hmd.putSkin();
+
+	}
+
+}
