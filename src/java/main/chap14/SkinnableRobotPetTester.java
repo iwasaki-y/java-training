d01be11e09f16900cb79f693c64a2384ef89207e
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-26 17:06:16 +0900

    chap14,4,4追加

---
+++ src/java/main/chap14/SkinnableRobotPetTester.java
@@ -0,0 +1,31 @@
+package chap14;
+
+public class SkinnableRobotPetTester {
+	
+	static void intro(Pet p) {
+		p.introduce();
+	}
+
+	public static void main(String[] args) {
+		Pet[] a = {
+				new Pet("Kurt", "アイ"),
+				new RobotPet("R2D2", "ルーク"),
+				new SkinnableRobotPet("OSX5", "Apple", Skinnable.LEOPARD),
+				new Pet("マイケル","英男"),
+		};
+		
+		for (Pet p : a) {
+			intro(p);
+			
+			if (p instanceof SkinnableRobotPet) {
+				System.out.print("スキンは");
+				((SkinnableRobotPet)p).printSkin();
+				System.out.println("です。");
+			}
+			
+			System.out.println();
+		}
+
+	}
+
+}
