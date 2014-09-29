0bb7cd61d0398417c0b9f70963ba6d6e65a86ccf
iwasaki-y <iwasaki-y@applicative.co.jp>
2014-09-29 10:39:14 +0900

    chap15,10,2追加

---
+++ src/java/main/chap15/PrintArray2.java
@@ -0,0 +1,34 @@
+package chap15;
+
+import java.util.Scanner;
+
+public class PrintArray2 {
+	
+	static void printString(String s) {
+		for (int i = 0; i < s.length(); i++)
+			System.out.print(s.charAt(i));
+	}
+	
+	static void printStringArray(String[] a) {
+		for (int i = 0; i < a.length; i++) {
+			printString(a[i]);
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		
+		System.out.print("文字列の個数:");
+		int n = stdIn.nextInt();
+		String[] sx = new String[n];
+		
+		for (int i = 0; i < sx.length; i++) {
+			System.out.print("sx[" + i + "] = ");
+			sx[i] = stdIn.next();
+		}
+		
+		printStringArray(sx);
+	}
+
+}
